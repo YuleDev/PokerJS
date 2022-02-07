a1ea5f05c2edb99c3752d232a1bb3a3b9bb72cea
thatblondetitan <koleervine@gmail.com>
2022-02-07 11:30:24 -0700

    removed old card array from js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-let deckArr = ["2 of Spades", "3 of of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades"
-    , "2 Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts"
-    , "2 Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 Clubs", "9 of Clubs", "10 of Clubs"
-    , "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds",
-    "J of Diamonds", "Q of Diamonds", "K of Diamonds", "A of Diamonds", "J of Clubs", "Q of Clubs", "K of Clubs", "A of Clubs", "J of Hearts", "Q of Hearts", "K of Hearts", "A of Hearts", "J of Spades", "Q of Spades", "K of Spades", "A of Spades"];
-
 const deckImagesArr = ["assets/images/deckOfCards/2_of_clubs.png", "assets/images/deckOfCards/2_of_diamonds.png",
     "assets/images/deckOfCards/2_of_hearts.png", "assets/images/deckOfCards/2_of_spades.png", "assets/images/deckOfCards/3_of_clubs.png",
     "assets/images/deckOfCards/3_of_diamonds.png", "assets/images/deckOfCards/3_of_hearts.png", "assets/images/deckOfCards/3_of_spades.png",
