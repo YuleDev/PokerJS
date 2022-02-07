79bfddbeba2c760acef9985d685aa46e4a9a3c56
thatblondetitan <koleervine@gmail.com>
2022-02-06 20:19:06 -0700

    js changes and new route for playing
    added card deck images for more realism

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-let deckArr = ["1 of Spades", "2 of Spades", "3 of of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades"
-,"1 of Hearts", "2 Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts"
-,"1 of Clubs", "2 Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 Clubs", "9 of Clubs", "10 of Clubs"
-,"1 of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds",
+let deckArr = ["2 of Spades", "3 of of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades"
+,"2 Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts"
+,"2 Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 Clubs", "9 of Clubs", "10 of Clubs"
+,"2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds",
 "J of Diamonds", "Q of Diamonds", "K of Diamonds", "A of Diamonds","J of Clubs", "Q of Clubs", "K of Clubs", "A of Clubs","J of Hearts", "Q of Hearts", "K of Hearts", "A of Hearts","J of Spades", "Q of Spades", "K of Spades", "A of Spades"];
 
 const startingButtonEl = document.createElement("button");
@@ -55,9 +55,19 @@ let aDangerousGame = function() {
     startingDivEl.appendChild(foldCardsEl);
     hitMeEl.addEventListener("click", flipAnotherCard);
     foldCardsEl.addEventListener("click", lostMoneyCowPoke);
+
+    if (dealerHand == 5) {
+        /* check dealer hand and player hand to find a score */
+    }
 };
 
 startingButtonEl.addEventListener("click", aDangerousGame);
+
+/* create a new array of images sources
+use a for loop to assign image sources to appended card images?
+have the dealer and player hands draw from the array of image sources 
+create img elements and assign them the corresponding sources
+have the button for more cards create new images and assign new sources*/
 
 
 /* on start of site have wanna play a game button display
