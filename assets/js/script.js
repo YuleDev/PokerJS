06b4e5b91e9ad20ef72000e3a7d4c7e25e544e04
thatblondetitan <koleervine@gmail.com>
2022-02-07 10:07:00 -0700

    added player hand, working on dealerhand

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,66 @@
 let deckArr = ["2 of Spades", "3 of of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades"
-,"2 Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts"
-,"2 Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 Clubs", "9 of Clubs", "10 of Clubs"
-,"2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds",
-"J of Diamonds", "Q of Diamonds", "K of Diamonds", "A of Diamonds","J of Clubs", "Q of Clubs", "K of Clubs", "A of Clubs","J of Hearts", "Q of Hearts", "K of Hearts", "A of Hearts","J of Spades", "Q of Spades", "K of Spades", "A of Spades"];
+    , "2 Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts"
+    , "2 Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 Clubs", "9 of Clubs", "10 of Clubs"
+    , "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds",
+    "J of Diamonds", "Q of Diamonds", "K of Diamonds", "A of Diamonds", "J of Clubs", "Q of Clubs", "K of Clubs", "A of Clubs", "J of Hearts", "Q of Hearts", "K of Hearts", "A of Hearts", "J of Spades", "Q of Spades", "K of Spades", "A of Spades"];
+
+const deckImagesArr = ["assets/images/deckOfCards/2_of_clubs.png", "assets/images/deckOfCards/2_of_diamonds.png",
+    "assets/images/deckOfCards/2_of_hearts.png", "assets/images/deckOfCards/2_of_spades.png", "assets/images/deckOfCards/3_of_clubs.png",
+    "assets/images/deckOfCards/3_of_diamonds.png", "assets/images/deckOfCards/3_of_hearts.png", "assets/images/deckOfCards/3_of_spades.png",
+    "assets/images/deckOfCards/4_of_clubs.png", "assets/images/deckOfCards/4_of_diamonds.png", "assets/images/deckOfCards/4_of_hearts.png",
+    "assets/images/deckOfCards/4_of_spades.png", "assets/images/deckOfCards/5_of_clubs.png", "assets/images/deckOfCards/5_of_diamonds.png",
+    "assets/images/deckOfCards/5_of_hearts.png", "assets/images/deckOfCards/5_of_spades.png", "assets/images/deckOfCards/6_of_clubs.png",
+    "assets/images/deckOfCards/6_of_diamonds.png", "assets/images/deckOfCards/6_of_hearts.png", "assets/images/deckOfCards/6_of_spades.png",
+    "assets/images/deckOfCards/7_of_clubs.png", "assets/images/deckOfCards/7_of_diamonds.png", "assets/images/deckOfCards/7_of_hearts.png",
+    "assets/images/deckOfCards/7_of_spades.png", "assets/images/deckOfCards/8_of_clubs.png", "assets/images/deckOfCards/8_of_diamonds.png",
+    "assets/images/deckOfCards/8_of_hearts.png", "assets/images/deckOfCards/8_of_spades.png", "assets/images/deckOfCards/9_of_clubs.png",
+    "assets/images/deckOfCards/9_of_diamonds.png", "assets/images/deckOfCards/9_of_hearts.png", "assets/images/deckOfCards/9_of_spades.png",
+    "assets/images/deckOfCards/10_of_clubs.png", "assets/images/deckOfCards/10_of_diamonds.png", "assets/images/deckOfCards/10_of_hearts.png",
+    "assets/images/deckOfCards/10_of_spades.png", "assets/images/deckOfCards/jack_of_clubs.png", "assets/images/deckOfCards/jack_of_diamonds.png",
+    "assets/images/deckOfCards/jack_of_hearts.png", "assets/images/deckOfCards/jack_of_spades.png", "assets/images/deckOfCards/queen_of_clubs.png",
+    "assets/images/deckOfCards/queen_of_diamonds.png", "assets/images/deckOfCards/queen_of_hearts.png", "assets/images/deckOfCards/queen_of_spades.png",
+    "assets/images/deckOfCards/king_of_clubs.png", "assets/images/deckOfCards/king_of_diamonds.png", "assets/images/deckOfCards/king_of_hearts.png",
+    "assets/images/deckOfCards/king_of_spades.png", "assets/images/deckOfCards/ace_of_clubs.png", "assets/images/deckOfCards/ace_of_diamonds.png",
+    "assets/images/deckOfCards/ace_of_hearts.png", "assets/images/deckOfCards/ace_of_spades.png"];
 
 const startingButtonEl = document.createElement("button");
 const startingDivEl = document.getElementById("startMatchContainer");
 startingButtonEl.textContent = "Wanna play a game partner?";
 startingButtonEl.id = "startMatchButton";
-let dealerHand = document.createElement("h1");
+let dealerHand = document.createElement("img");
 dealerHand.classList.add("dealerHand");
-let playerHand = document.createElement("h1");
-playerHand.classList.add("playerHand");
+let playerCard1 = document.createElement("img");
+let playerCard2 = document.createElement("img");
+playerCard1.classList.add("playerHand");
+playerCard2.classList.add("playerHand");
 const hitMeEl = document.createElement("button");
 hitMeEl.textContent = "Hit me partner!";
 const foldCardsEl = document.createElement("button");
 foldCardsEl.textContent = "I'm backin out pardner";
 
-function getStartingHand(deckArr, num) {
-    const shuffled = [...deckArr].sort(() => 0.5 - Math.random());
+function getStartingHand(deckImagesArr, num) {
+    const shuffled = [...deckImagesArr].sort(() => 0.5 - Math.random());
 
     return shuffled.slice(0, num);
 };
 
-console.log(getStartingHand(deckArr, 2));
+console.log(getStartingHand(deckImagesArr, 2));
 
-let dealWithTheDevil = function() {
+let dealWithTheDevil = function () {
     startingDivEl.appendChild(startingButtonEl);
 };
 
-let flipAnotherCard = function() {
-    /* grab another card from the deck, add it into displayed dealer cards */
-    dealerHand.insertAdjacentText("beforeend", "," + getStartingHand(deckArr, 1))
+let flipAnotherCard = function () {
+    /* dealerHand.insertAdjacentText("beforeend", "," + getStartingHand(deckArr, 1)) */
+    /* startingDivEl.appendChild(dealerHand);
+    dealerHand.setAttribute("src", (getStartingHand(deckImagesArr, 1))); */
+
 }
 
-let lostMoneyCowPoke = function() {
+let lostMoneyCowPoke = function () {
     startingDivEl.removeChild(dealerHand);
-    startingDivEl.removeChild(playerHand);
+    startingDivEl.removeChild(playerCard1);
+    startingDivEl.removeChild(playerCard2);
     startingDivEl.removeChild(hitMeEl);
     startingDivEl.removeChild(foldCardsEl);
     dealWithTheDevil();
@@ -44,15 +68,25 @@ let lostMoneyCowPoke = function() {
 
 dealWithTheDevil();
 
-let aDangerousGame = function() {
+let aDangerousGame = function () {
     startingDivEl.removeChild(startingButtonEl);
-    dealerHand.innerHTML = (getStartingHand(deckArr, 3));
-    startingDivEl.appendChild(dealerHand);
-    playerHand.innerHTML = (getStartingHand(deckArr, 2));
-    startingDivEl.appendChild(playerHand);
+
+    /* dealerHand.innerHTML = (getStartingHand(deckArr, 3)); */
+
+    for (let i = 0; i < (getStartingHand(deckImagesArr, 3)).length; i++) {
+        startingDivEl.appendChild(dealerHand)[i];
+        dealerHand.setAttribute("src", (getStartingHand(deckImagesArr)[i]));
+    }
+
+    startingDivEl.appendChild(playerCard1);
+    startingDivEl.appendChild(playerCard2);
+    playerCard1.setAttribute("src", (getStartingHand(deckImagesArr, 1)));
+    playerCard2.setAttribute("src", (getStartingHand(deckImagesArr, 1)));
+
 
     startingDivEl.appendChild(hitMeEl);
     startingDivEl.appendChild(foldCardsEl);
+
     hitMeEl.addEventListener("click", flipAnotherCard);
     foldCardsEl.addEventListener("click", lostMoneyCowPoke);
 
@@ -65,24 +99,6 @@ startingButtonEl.addEventListener("click", aDangerousGame);
 
 
 
-const deckImagesArr = ["assets/images/deckOfCards/2_of_clubs.png", "assets/images/deckOfCards/2_of_diamonds.png",
-"assets/images/deckOfCards/2_of_hearts.png","assets/images/deckOfCards/2_of_spades.png","assets/images/deckOfCards/3_of_clubs.png",
-"assets/images/deckOfCards/3_of_diamonds.png","assets/images/deckOfCards/3_of_hearts.png","assets/images/deckOfCards/3_of_spades.png",
-"assets/images/deckOfCards/4_of_clubs.png","assets/images/deckOfCards/4_of_diamonds.png","assets/images/deckOfCards/4_of_hearts.png",
-"assets/images/deckOfCards/4_of_spades.png","assets/images/deckOfCards/5_of_clubs.png","assets/images/deckOfCards/5_of_diamonds.png",
-"assets/images/deckOfCards/5_of_hearts.png","assets/images/deckOfCards/5_of_spades.png","assets/images/deckOfCards/6_of_clubs.png",
-"assets/images/deckOfCards/6_of_diamonds.png","assets/images/deckOfCards/6_of_hearts.png","assets/images/deckOfCards/6_of_spades.png",
-"assets/images/deckOfCards/7_of_clubs.png","assets/images/deckOfCards/7_of_diamonds.png","assets/images/deckOfCards/7_of_hearts.png",
-"assets/images/deckOfCards/7_of_spades.png","assets/images/deckOfCards/8_of_clubs.png","assets/images/deckOfCards/8_of_diamonds.png",
-"assets/images/deckOfCards/8_of_hearts.png","assets/images/deckOfCards/8_of_spades.png","assets/images/deckOfCards/9_of_clubs.png",
-"assets/images/deckOfCards/9_of_diamonds.png","assets/images/deckOfCards/9_of_hearts.png","assets/images/deckOfCards/9_of_spades.png",
-"assets/images/deckOfCards/10_of_clubs.png","assets/images/deckOfCards/10_of_diamonds.png","assets/images/deckOfCards/10_of_hearts.png",
-"assets/images/deckOfCards/10_of_spades.png","assets/images/deckOfCards/jack_of_clubs.png","assets/images/deckOfCards/jack_of_diamonds.png",
-"assets/images/deckOfCards/jack_of_hearts.png","assets/images/deckOfCards/jack_of_spades.png","assets/images/deckOfCards/queen_of_clubs.png",
-"assets/images/deckOfCards/queen_of_diamonds.png","assets/images/deckOfCards/queen_of_hearts.png","assets/images/deckOfCards/queen_of_spades.png",
-"assets/images/deckOfCards/king_of_clubs.png","assets/images/deckOfCards/king_of_diamonds.png","assets/images/deckOfCards/king_of_hearts.png",
-"assets/images/deckOfCards/king_of_spades.png","assets/images/deckOfCards/ace_of_clubs.png","assets/images/deckOfCards/ace_of_diamonds.png",
-"assets/images/deckOfCards/ace_of_hearts.png","assets/images/deckOfCards/ace_of_spades.png"];
 
 
 
